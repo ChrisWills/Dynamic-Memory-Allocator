6aead056aed5340726423f7a38f248338304f893
Christian Wills <cwills.dev@gmail.com>
2012-04-04 21:01:52 -0400

    Added description of compile time preprocessor directives and wrapped debug functions in ifdef

--- malloc.h
+++ malloc.h
@@ -1,7 +1,19 @@
+/*
+	** Compile time options **
+	
+	Option						Default_Value			Description
+	--------------------------------------------------------------------------------------------
+	MALLOC_DEBUG				NOT DEFINED				Enables debugging functions when defined
+	MALLOC_DETECT_DOUBLE_FREE	NOT_DEFINED				Enabled double free detection when defined at the expense of free() runtime performance
+
+ */
+
 #include <stddef.h>
 
 void *my_malloc(size_t size);
 
 void my_free(void *ptr);
 
+#ifdef MALLOC_DEBUG
 void print_free_list(void);
+#endif
